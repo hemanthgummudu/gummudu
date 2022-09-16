cad9270d03e6bb5ca1e20326ede7a95da0dc3641
hemanthgummudu <113668912+hemanthgummudu@users.noreply.github.com>
2022-09-16 14:58:42 +0530

    16 sep 2022

---
+++ 1.py
@@ -0,0 +1,12 @@
+l=["bulbasur_21","coocoo_54","squirtle_42","charmender_09","groudon_69","seviper_68","paras_00","koko_23"]
+l2=[]
+for i in l[::2]:
+    l2.append(i)
+    print(i)
+    
+
+    
+   
+    
+
+    
